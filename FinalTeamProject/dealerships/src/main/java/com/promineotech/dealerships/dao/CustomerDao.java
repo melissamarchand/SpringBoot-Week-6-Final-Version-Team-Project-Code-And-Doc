87e4f7c3a5799051c407ed2d5d89648bbd12479e
Melissa Beth Marchand <mb.marchand@icloud.com>
2021-06-08 17:46:05 -0400

    Update CustomerDao.java

--- FinalTeamProject/dealerships/src/main/java/com/promineotech/dealerships/dao/CustomerDao.java
+++ FinalTeamProject/dealerships/src/main/java/com/promineotech/dealerships/dao/CustomerDao.java
@@ -16,8 +16,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomerDao {
     private static final String HOSTNAME = "jdbc:mysql://localhost:3306/dealership?useSSL=false";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "password1";
+    private static final String USERNAME = "";
+    private static final String PASSWORD = "";
 
     public List<Customer> listAllCustomers() {
         final String sql = "SELECT * FROM customers";
